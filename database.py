0dd59c63021f259e4a5777d092cde387cdc1009e
kimyinclone-design <kimyinclone@gmail.com>
2026-08-25 22:09:49 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,44 @@
 
+import os
+import sqlite3
+
+if os.name == 'nt':
+    DB_PATH = r"D:\shop.db"
+else:
+    DB_PATH = "shop.db"
+
+def init_db():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS orders (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            username TEXT,
+            full_name TEXT,
+            item_name TEXT,
+            price TEXT,
+            customer_info TEXT,
+            date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+def save_order(user_id, username, full_name, item_name, price, customer_info):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO orders (user_id, username, full_name, item_name, price, customer_info)
+        VALUES (?, ?, ?, ?, ?, ?)
+    ''', (user_id, username, full_name, item_name, price, customer_info))
+    conn.commit()
+    conn.close()
+
+def get_user_orders(user_id):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("SELECT item_name, price, date FROM orders WHERE user_id = ?", (user_id,))
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
